9b225b164ca9d63d4da7006eee9df64a5553684e
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-10-01 12:18:09 -0500

    changed some CSS

--- script.js
+++ script.js
@@ -1,13 +1,116 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+(function(){
+  function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
 
-function buildQuiz(){}
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
 
-function showResults(){}
+        // variable to store the list of possible answers
+        const answers = [];
 
-// display quiz right away
-buildQuiz();
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
 
-// on submit, show results
-submitButton.addEventListener('click', showResults);
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
+
+  function showResults(){
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if(userAnswer === currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+
+//quiz questions and answers
+  const myQuestions = [
+    {
+      question: "Is a variable named 'apple' same as 'Apple' in javascript?",
+      answers: {
+        a: "Yes",
+        b: "No",
+        c: "Only when we use 'strict'",
+        d: "None of the above"
+      },
+      correctAnswer: "b"
+    },
+
+    {
+      question: "Inside which HTML element do we put the JavaScript <...>?",
+      answers: {
+        a: "script",
+        b: "javascript",
+        c: "js"
+      },
+      correctAnswer: "a"
+    },
+
+    {
+      question: "Where is the correct place to insert a JavaScript?",
+      answers: {
+        a: "The <body> section",
+        b: "The <head> section",
+        c: "Both the <head> section and the <body> section are correct"
+      },
+      correctAnswer: "c"
+    }
+  ];
+
+  // Kick things off
+  buildQuiz();
+
+  // Event listeners
+  submitButton.addEventListener('click', showResults);
+})();
